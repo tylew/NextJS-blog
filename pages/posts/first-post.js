c46e13c59a01e161f882684d3f7e7f9e5e988f25
tylew <tylerlewissb@gmail.com>
2022-04-26 19:09:44 -0700

    Full commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+    <Layout>
+      <Head><title>Hello</title></Head>
+      
+      <h1>Hello</h1>
+    </Layout>
   )
 }
